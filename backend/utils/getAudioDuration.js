6a53a472d22f69585a5fc0ff52320b327b25ddd7
Lonny75 <lonnbevert@yahoo.fr>
2025-08-27 11:27:35 +0200

    Delete backend/utils directory

--- backend/utils/getAudioDuration.js
+++
@@ -1,9 +0,0 @@
-import { exec } from "child_process";
-import { promisify } from "util";
-
-const execPromise = promisify(exec);
-
-export async function getAudioDuration(filePath) {
-  const { stdout } = await execPromise(`ffprobe -i ${filePath} -show_entries format=duration -v quiet -of csv="p=0"`);
-  return parseFloat(stdout);
-}

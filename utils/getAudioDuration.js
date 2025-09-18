070939d7aef62f940cf8d607e1206cf95c0a4e5d
Lonny75 <lonnbevert@yahoo.fr>
2025-09-18 15:20:55 +0200

    Update getAudioDuration.js

--- utils/getAudioDuration.js
+++ utils/getAudioDuration.js
@@ -1,17 +1,13 @@
-// utils/getAudioDuration.js
 import ffmpeg from "fluent-ffmpeg";
 
-/**
- * Renvoie la durée d'un fichier audio en secondes
- * @param {string} filePath - Chemin vers le fichier audio
- * @returns {Promise<number>} - Durée en secondes
- */
-export default function getAudioDuration(filePath) {
+export async function getAudioDuration(filePath) {
   return new Promise((resolve, reject) => {
     ffmpeg.ffprobe(filePath, (err, metadata) => {
-      if (err) return reject(err);
-      const duration = metadata.format.duration;
-      resolve(duration);
+      if (err) {
+        reject(err);
+      } else {
+        resolve(metadata.format.duration);
+      }
     });
   });
 }
